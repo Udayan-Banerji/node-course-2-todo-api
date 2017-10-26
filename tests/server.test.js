9e7ab9f0d59004c7a59f346ce53c4c06bb565d0b
Udayan-Banerji <udayanban@gmail.com>
2017-10-26 16:51:05 +0530

    Add tests for GET /todos

--- tests/server.test.js
+++ tests/server.test.js
@@ -5,58 +5,84 @@ const request = require('supertest');
 const {app} = require('./../server/server');
 const {Todo} = require('./../server/models/todo');
 
+const todos = [{
+  text: "first test todo"
+}, {
+  text: "second test todo"
+}];
+
+
+
 beforeEach((done) => {
   Todo.remove({}).then(() => {
-    done();
-  });
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () =>{
-  it('should createe a new todo', (done) => {
-    var text = 'Test todo text';
+  it('should create a new todo', (done) => {
+    var testText = 'Test todo text';
 
     request(app)          //using supertest
       .post('/todos')
-      .send({text})      //gets converted to JSON by supertest
+      .send({text: testText})      //gets converted to JSON by supertest
       .expect(200)
       .expect((res) => {
-        expect(res.body.text).toBe(text);
+        expect(res.body.text).toBe(testText);
       })
       .end((err,res) => {
         if (err) {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text: testText}).then((todos) => {
           expect(todos.length).toBe(1);
-          expect(todos[0].text).toBe(text);
+          expect(todos[0].text).toBe(testText);
           done();
 
         }).catch((e) => {done(e)});
       });
     });
 
-    it('should not create todo with invalid data', () => {
-      var text = '';
+    it('should not create todo with invalid data', (done) => {
+      var testText = '';
+      console.log("*********Got there****************1");
+
+          request(app)          //using supertest
+            .post('/todos')
+            .send({text: testText})      //gets converted to JSON by supertest
+            .expect(400)
+            // .expect((res) => {
+            //   expect(res.body.text).toNotBe(text);
+            // })
+            .end((err,res) => {
+              if (err) {
+                return done(err);
+              }
+
+              Todo.find().then((todos) => {
+                expect(todos.length).toBe(2);
+                //expect(todos[0].text).toBe(testText);  //can't check for ""
+                done();
+
+              }).catch((e) => {done(e)});
+      });
+
+      console.log("*********Got there****************2");
+    });
+
+  });
+
+  describe('Get /todos', () => {
+    it('should get the 2 test todos',(done) => {
 
       request(app)
-      .post('todos')
-      .send({text})
-      .expect(400)
-      .expect((res)=>{
-        expect(res.body.text).toNoBe(text)   //see the MJacson github expect
-      })
-      .end((err, res) => {
-        if(err) {
-          return done(err);
-        }
-
-        Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
-          done();
-
-        }).catch((e)=> {done(e)} );
-      });
+        .get('/todos')
+        .expect(200)
+        .expect((res)=>{                            //custom assertion
+          expect(res.body.todos.length).toBe(2);
+        })
+        .end(done);
     });
 
   });
